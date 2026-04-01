dbdd9e2082b19294ff5c3e37666666ff0096398a
liang.geng <liang.geng@mthreads.com>
2026-04-01 21:20:54 +0800

    del import  musa_patch in entrance

--- swift/cli/_megatron/pt.py
+++ swift/cli/_megatron/pt.py
@@ -1,9 +1,6 @@
 # Copyright (c) ModelScope Contributors. All rights reserved.
 import os
 
-if os.getenv("ACCELERATOR_BACKEND") == "musa":
-    import musa_patch
-
 if __name__ == '__main__':
     os.environ.setdefault('CUDA_DEVICE_MAX_CONNECTIONS', '1')
     from swift.megatron import megatron_pretrain_main
